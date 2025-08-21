4e751e45aa8c957b150eb7dce86142792d02e21f
Boxzhi <coderhzz@163.com>
2025-08-21 17:57:59 +0800

    update

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "HZNavigationBar",
             path: "Sources",
-            exclude: ["../Example", "../Images"],
+            exclude: ["./Example", "./Images"],
             sources: ["."]
         )
     ],
